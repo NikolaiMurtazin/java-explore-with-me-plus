0559700977a67543896ec25a51847147dc291243
Roman <glazinet1209@yandex.ru>
2024-08-22 13:33:28 +0300

    Реализовал получение статистики

--- stats/stats-dto/src/main/java/ru/practicum/stat/EndpointHitDTO.java
+++ stats/stats-dto/src/main/java/ru/practicum/stat/EndpointHitDTO.java
@@ -1,12 +1,20 @@
 package ru.practicum.stat;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EndpointHitDTO {
     @NotEmpty(message = "App name can't be empty")
     private String app;
@@ -18,6 +26,7 @@ public class EndpointHitDTO {
     @Pattern(regexp = "^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$", message = "Invalid IP address format")
     private String ip;
 
-    @NotEmpty(message = "Timestamp can't be empty")
-    private String timestamp;
+    @NotNull(message = "Timestamp can't be empty")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
 }
---
+++ stats/stats-service/src/main/java/ru/practicum/controller/StatController.java
@@ -0,0 +1,49 @@
+package ru.practicum.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.service.StatService;
+import ru.practicum.stat.EndpointHitDTO;
+import ru.practicum.stat.StatsParams;
+import ru.practicum.stat.ViewStatsDTO;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class StatController {
+
+    private final StatService statService;
+
+    @PostMapping("/hit")
+    public EndpointHitDTO saveStats(@Valid @RequestBody
+                                    EndpointHitDTO hitDto) {
+        return statService.save(hitDto);
+    }
+
+
+    @GetMapping("/stats")
+    public List<ViewStatsDTO> getStats(@RequestParam("start") String start,
+                                       @RequestParam("end") String end,
+                                       @RequestParam(value = "uris") List<String> uris,
+                                       @RequestParam(value = "unique", defaultValue = "false") Boolean unique) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String decodedStartDate = URLDecoder.decode(start, StandardCharsets.UTF_8);
+        String decodedEndDate = URLDecoder.decode(end, StandardCharsets.UTF_8);
+        LocalDateTime startDecode = LocalDateTime.parse(decodedStartDate, formatter);
+        LocalDateTime endDecode = LocalDateTime.parse(decodedEndDate, formatter);
+        StatsParams statsParams = StatsParams.builder()
+                .start(startDecode)
+                .end(endDecode)
+                .unique(unique)
+                .uris(uris)
+                .build();
+
+        return statService.getStats(statsParams);
+    }
+}
