abb59066d5d1730b5728f41fd4bb89c468493db9
Nikolai Murtazin <147885890+NikolaiMurtazin@users.noreply.github.com>
2024-08-23 12:38:37 +0200

    Revert "Исправил getStats"
    This reverts commit 48168ed28753bf127d1c9a2b5fe1d571e69fb372.

--- stats/stats-server/src/main/java/ru/practicum/service/GeneralStatService.java
+++ stats/stats-server/src/main/java/ru/practicum/service/GeneralStatService.java
@@ -41,9 +41,9 @@ public class GeneralStatService implements StatService {
         }
 
         if (urisPresent) {
-            return statRepository.findByNonUniqueIp(params.getStart(), params.getEnd(), params.getUris());
+            return statRepository.findByNotUniqueIp(params.getStart(), params.getEnd(), params.getUris());
         }
 
-        return statRepository.findAllByNonUniqueIp(params.getStart(), params.getEnd());
+        return statRepository.findAllByNotUniqueIp(params.getStart(), params.getEnd());
     }
 }
