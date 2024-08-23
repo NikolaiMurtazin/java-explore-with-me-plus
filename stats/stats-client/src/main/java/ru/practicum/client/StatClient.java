eabc0673108bd54e2525b8d312f181d75a98dd46
Nikolai Murtazin <147885890+NikolaiMurtazin@users.noreply.github.com>
2024-08-23 12:38:37 +0200

    Revert ""fix: изменения, чтобы прошли тесты на GitHub""
    This reverts commit 438075d0a0367d97b0cf32104c98f8f544495d79.

--- stats/stats-client/src/main/java/ru/practicum/client/StatClient.java
+++ stats/stats-client/src/main/java/ru/practicum/client/StatClient.java
@@ -1,7 +1,24 @@
 package ru.practicum.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 
 @Component
 public class StatClient {
+    final RestClient client;
+    final String statUrl;
+
+    @Autowired
+    public StatClient(RestClient client, @Value("${client.url") String startUrl) {
+        this.client= client;
+        this.statUrl = startUrl;
+    }
+
+    protected
+    String result = restClient.get()
+            .uri(uriBase + "/articles")
+            .retrieve()
+            .body(String.class);
 }
