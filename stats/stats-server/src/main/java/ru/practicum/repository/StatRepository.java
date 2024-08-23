7feb24dbce4854e438abcebc5f3a867029559169
Nikolai Murtazin <147885890+NikolaiMurtazin@users.noreply.github.com>
2024-08-23 12:38:37 +0200

    Revert "Исправил getStats"
    This reverts commit 48168ed28753bf127d1c9a2b5fe1d571e69fb372.

--- stats/stats-server/src/main/java/ru/practicum/repository/StatRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/repository/StatRepository.java
@@ -13,33 +13,26 @@ import java.util.List;
 @Repository
 public interface StatRepository extends JpaRepository<EndpointHit, Long> {
 
-    @Query("SELECT new ru.practicum.stat.ViewStatsDTO(e.app, e.uri, COUNT(DISTINCT e.ip)) " +
-            "FROM EndpointHit e " +
-            "WHERE e.timestamp between :start AND :end " +
-            "GROUP BY e.app, e.uri " +
-            "ORDER By COUNT(DISTINCT e.ip) DESC")
-    List<ViewStatsDTO> findAllByUniqueIp(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+    @Query(value = "SELECT app, uri,  COUNT(DISTINCT ip) as hits FROM endpointhit e " +
+            "WHERE e.timestamp between ?1 AND ?2 " +
+            "GROUP BY app, uri", nativeQuery = true)
+    List<ViewStatsDTO> findAllByUniqueIp(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT new ru.practicum.stat.ViewStatsDTO(e.app, e.uri, COUNT(e.ip)) " +
-            "FROM EndpointHit e " +
-            "WHERE e.timestamp between :start AND :end " +
-            "GROUP BY e.app, e.uri " +
-            "ORDER By COUNT(e.ip) DESC")
-    List<ViewStatsDTO> findAllByNonUniqueIp(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+    @Query(value = "SELECT app, uri,  COUNT(ip) as hits FROM endpointhit e " +
+            "WHERE e.timestamp between ?1 AND ?2 " +
+            "GROUP BY app, uri", nativeQuery = true)
+    List<ViewStatsDTO> findAllByNotUniqueIp(LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.stat.ViewStatsDTO(e.app, e.uri, COUNT(DISTINCT e.ip)) " +
             "FROM EndpointHit e " +
             "WHERE e.timestamp between :start AND :end AND e.uri IN :uris " +
-            "GROUP BY e.app, e.uri " +
-            "ORDER By COUNT(DISTINCT e.ip) DESC")
+            "GROUP BY e.app, e.uri")
     List<ViewStatsDTO> findByUniqueIp(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end, @Param("uris") List<String> uris);
 
 
-    @Query("SELECT new ru.practicum.stat.ViewStatsDTO(e.app, e.uri, COUNT(e.ip)) " +
+    @Query("SELECT new ru.practicum.stat.ViewStatsDTO(e.app, e.uri, COUNT(DISTINCT e.ip)) " +
             "FROM EndpointHit e " +
-            "WHERE e.timestamp between :start AND :end AND e.uri IN :uris " +
-            "GROUP BY e.app, e.uri " +
-            "ORDER By COUNT(e.ip) DESC")
-    List<ViewStatsDTO> findByNonUniqueIp(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end, @Param("uris") List<String> uris);
-
+            "WHERE (e.timestamp between ?1 AND ?2) AND e.uri IN ?3 " +
+            "GROUP BY e.app, e.uri")
+    List<ViewStatsDTO> findByNotUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uri);
 }
