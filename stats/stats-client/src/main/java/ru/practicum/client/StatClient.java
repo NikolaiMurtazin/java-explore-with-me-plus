af120e3ab81c16b2dde444d7c2ed52ecd9ac7541
Nikolai Murtazin <murtazinnikol@yandex.ru>
2024-09-07 17:03:53 +0200

    "fix: change RestTemplate to WebClient"

--- stats/stats-client/src/main/java/ru/practicum/client/StatClient.java
+++ stats/stats-client/src/main/java/ru/practicum/client/StatClient.java
@@ -5,11 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import ru.practicum.stat.EndpointHitDTO;
 import ru.practicum.stat.StatsParams;
@@ -20,19 +16,25 @@ import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
 @Slf4j
 @Component
 public class StatClient {
-    private final RestTemplate restTemplate;
+
+    private final WebClient webClient;
     private final String startUrl;
 
     @Autowired
     public StatClient(@Value("${client.url}") String startUrl) {
         this.startUrl = startUrl;
-        var factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectTimeout(10000);
-        factory.setConnectionRequestTimeout(10000);
-        restTemplate = new RestTemplate(factory);
+        this.webClient = WebClient.builder()
+                .baseUrl(startUrl)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
     }
 
     public void saveStats(HttpServletRequest request) {
@@ -46,10 +48,15 @@ public class StatClient {
                     .timestamp(LocalDateTime.now())
                     .build();
 
-            restTemplate.postForObject(startUrl + "/hit", dto, String.class);
+            webClient.post()
+                    .uri("/hit")
+                    .bodyValue(dto)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
 
         } catch (Exception e) {
-            log.warn(e.getMessage());
+            log.warn("Failed to save stats: {}", e.getMessage());
         }
     }
 
@@ -59,13 +66,16 @@ public class StatClient {
                     .queryParam("start", encodeDate(params.getStart()))
                     .queryParam("end", encodeDate(params.getEnd()))
                     .queryParam("uris", params.getUris())
-                    .queryParam("unique", params.getUnique()).toUriString();
-            ResponseEntity<List<ViewStatsDTO>> response = restTemplate.exchange(startUrl + uri,
-                    HttpMethod.GET, null, new ParameterizedTypeReference<List<ViewStatsDTO>>() {
-                    });
-            return response.getBody();
+                    .queryParam("unique", params.getUnique())
+                    .toUriString();
+
+            return webClient.get()
+                    .uri(uri)
+                    .retrieve()
+                    .bodyToMono(new ParameterizedTypeReference<List<ViewStatsDTO>>() {})
+                    .block();
         } catch (Exception e) {
-            log.warn(e.getMessage());
+            log.warn("Failed to get stats: {}", e.getMessage());
         }
         return Collections.emptyList();
     }
